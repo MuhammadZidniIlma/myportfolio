82282aa60e68f87c1bb86be4ff4bab7f0613d190
MuhammadZidniilma <m.zidniilma@gmail.com>
2025-12-21 17:04:29 +0700

    change desc about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -46,9 +46,7 @@ export default function About() {
         <div className="flex flex-col md:flex-row items-center justify-between mt-10">
           <div className="w-full md:w-1/2 pt-0 md:pt-20">
               <h1 className="text-3xl font-bold mb-4">Biography</h1>
-              <p className="text-justify">Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and innovative ways to bring my clients' visions to life.<br/>
-              I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users.<br/>
-              Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
+              <p className="text-justify">I'm a UI Developer and Full-Stack Developer with a strong passion for building modern web applications. As a skilled developer, I am dedicated to turning ideas into innovative, functional, and visually appealing digital solutions.<br/>With expertise in Laravel and web development technologies, I focus on creating intuitive user interfaces and scalable backend systems. Through my latest projects and articles, I continuously explore new technologies and share insights about web development.</p>
           </div>
           <div className=" flex justify-center order-first md:order-last">
             <div className="relative w-72 h-72 lg:w-96 lg:h-96 aspect-square mb-10 md:mb-0 md:hidden">
